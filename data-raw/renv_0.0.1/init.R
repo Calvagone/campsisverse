fb33148548c00934c34245d8a90caee3a7f810e8
luyckxn <nicolas.luyckx@calvagone.com>
2024-10-13 14:42:28 +0200

    Include campsisverse in renv_0.0.1

--- data-raw/renv_0.0.1/init.R
+++ data-raw/renv_0.0.1/init.R
@@ -5,7 +5,7 @@ project <- "data-raw/renv_0.0.1"
 # Note: click no when you are prompted to quit the session
 renv::init(project=project, repos="https://packagemanager.posit.co/cran/2024-09-30")
 
-# Configure options for tests
+# Configure options for tests (only useful on this machine, to install packages with tests)
 campsisverse::configureOptions()
 
 # Install open-source packages
@@ -14,6 +14,7 @@ renv::install(project=project,
                          "Calvagone/campsis@v1.5.4",
                          "Calvagone/campsisnca@v1.5.0",
                          "Calvagone/campsismisc@v0.5.0",
+                         "Calvagone/campsisverse@master", # To replace by the official release
                          "mrgsolve",
                          "rxode2",
                          "xgxr",
