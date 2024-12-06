02c66e65c5a13703eed585516f02547fdd81dba5
luyckxn <nicolas.luyckx@calvagone.com>
2024-12-06 15:47:08 +0100

    Delete old snapshots.

--- R/data.R
+++ R/data.R
@@ -1,15 +1,5 @@
 
 #'
-#' Renv lock file made on 241031.
-#' 
-"renv_lock_241031"
-
-#'
-#' Renv lock file made on 241126.
-#' 
-"renv_lock_241126"
-
-#'
 #' Renv lock file made on 241201.
 #' 
 "renv_lock_241201"
--- data-raw/renv_lock_to_data.R
+++ data-raw/renv_lock_to_data.R
@@ -1,10 +1,3 @@
 
-
-renv_lock_241031 <- readr::read_file("data-raw/renv_241031/renv.lock")
-usethis::use_data(renv_lock_241031, overwrite=TRUE)
-
-renv_lock_241126 <- readr::read_file("data-raw/renv_241126/renv.lock")
-usethis::use_data(renv_lock_241126, overwrite=TRUE)
-
 renv_lock_241201 <- readr::read_file("data-raw/renv_241201/renv.lock")
 usethis::use_data(renv_lock_241201, overwrite=TRUE)
