9015cf81d67bbe7b4dd07880052dcea0296c5fdc
luyckxn <nicolas.luyckx@calvagone.com>
2024-12-06 15:54:33 +0100

    #8 Update tests.

--- tests/testthat/testMain.R
+++ tests/testthat/testMain.R
@@ -6,11 +6,11 @@ source(paste0("", "testUtils.R"))
 
 test_that("Method 'processVersion' works as expected", {
 
-  expect_equal(processVersion("24-10-31"), "241031")
-  expect_equal(processVersion("24.10.31"), "241031")
+  expect_equal(processVersion("24-12-01"), "241201")
+  expect_equal(processVersion("24.12.01"), "241201")
   
-  expect_error(processVersion("24-10-30"), regexp="Version 24-10-30 is not available. Available versions are: 24-10-31")
-  expect_error(processVersion("24.10.30"), regexp="Version 24\\.10\\.30 is not available. Available versions are: 24-10-31")
+  expect_error(processVersion("24-10-31"), regexp="Version 24-10-31 is not available. Available versions are: 24-12-01")
+  expect_error(processVersion("24.10.31"), regexp="Version 24\\.10\\.31 is not available. Available versions are: 24-12-01")
 })
 
 test_that("Method 'getCampsisSuitePackages' works as expected", {
