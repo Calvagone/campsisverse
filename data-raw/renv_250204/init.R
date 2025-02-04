b837e9be9ea976ea5700494d74bdfd4337facaf6
luyckxn <nicolas.luyckx@calvagone.com>
2025-02-04 18:43:22 +0100

    #9 New lock file.

---
+++ data-raw/renv_250204/init.R
@@ -0,0 +1,34 @@
+repos <- "https://packagemanager.posit.co/cran/2025-02-03"
+options(repos=repos)
+
+# Install correct version of renv
+install.packages("renv")
+
+# Init
+renv::init(repos=repos)
+
+# Install tests by default
+options(INSTALL_opts="--install-tests")
+
+# Set up GitHub PAT
+
+# Install and snapshot (lock=TRUE)
+renv::install(
+  packages=c(
+    "Calvagone/campsismod@develop",
+    "Calvagone/campsis@develop",
+    "Calvagone/campsisnca@v1.5.1",
+    "Calvagone/campsismisc@v0.5.2",
+    "Calvagone/campsisqual@v1.3.1",
+    "Calvagone/campsistrans@v1.2.3",
+    "mrgsolve@1.5.1", # See https://github.com/Calvagone/campsis/issues/160
+    "rxode2",
+    "ncappc",         # Campsisnca testing
+    "xgxr",           # e-Campsis
+    "cowplot",        # e-Campsis
+    "ragg"            # High-quality 2D drawing library 
+    ),
+  rebuild=TRUE,
+  repos=repos,
+  lock=TRUE)
+
