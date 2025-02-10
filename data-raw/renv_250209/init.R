437c5f60b41b8263b6e1a9ec3fe9884f981d3be2
luyckxn <nicolas.luyckx@calvagone.com>
2025-02-10 09:18:16 +0100

    #9 Prepare release v25.02.09

--- data-raw/renv_250209/init.R
+++ data-raw/renv_250209/init.R
@@ -1,4 +1,4 @@
-repos <- "https://packagemanager.posit.co/cran/2025-02-03"
+repos <- "https://packagemanager.posit.co/cran/2025-02-09"
 options(repos=repos)
 
 # Install correct version of renv
@@ -6,6 +6,8 @@ install.packages("renv")
 
 # Init
 renv::init(repos=repos)
+
+# Reassign repos variable
 
 # Install tests by default
 options(INSTALL_opts="--install-tests")
@@ -15,11 +17,11 @@ options(INSTALL_opts="--install-tests")
 # Install and snapshot (lock=TRUE)
 renv::install(
   packages=c(
-    "Calvagone/campsismod@develop",
-    "Calvagone/campsis@develop",
+    "Calvagone/campsismod@v1.2.0",
+    "Calvagone/campsis@v1.6.0",
     "Calvagone/campsisnca@v1.5.1",
     "Calvagone/campsismisc@v0.5.2",
-    "Calvagone/campsisqual@v1.3.1",
+    "Calvagone/campsisqual@v1.3.2",
     "Calvagone/campsistrans@v1.2.3",
     "mrgsolve@1.5.1", # See https://github.com/Calvagone/campsis/issues/160
     "rxode2",
